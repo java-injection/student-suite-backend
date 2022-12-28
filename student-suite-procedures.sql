78b33b14f87a9a777949fa4c2e76d6a173aca9ff
Max <massimo.m.morgantini@gmail.com>
2022-12-28 10:34:07 +0100

    fixed insert tutors in _Load() procedure

--- student-suite-procedures.sql
+++ student-suite-procedures.sql
@@ -50,13 +50,11 @@ BEGIN
         ('oissalA', 'Pecilli', '2005-01-10');
 
 
-    INSERT INTO professori (nome, cognome, data_nascita)
+    INSERT INTO tutors (nome, cognome, data_nascita)
     VALUES ('Luca', 'Coraci', '1985-04-22'),
         ('Davide', 'Cascella', '2005-07-11'),
         ('Massimo Michele', 'Morgantini', '2005-01-10'),
         ('Alessio', 'Pecilli', '2003-07-13');
-
-
 
 END &&
 DELIMITER ;
