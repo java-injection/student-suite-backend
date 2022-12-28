47640b32cf1d89920b507724bfacec588d7fb9cc
davide <david.cascella.5@gmail.com>
2022-12-28 09:53:09 +0100

    create di procedure
    cleanup, shut_down, _load, _select

--- student-suite-procedures.sql
+++ student-suite-procedures.sql
@@ -1,1 +1,58 @@
 -- procedures
+
+ -- CLEANUP
+ DROP PROCEDURE IF EXISTS Cleanup;
+ 
+ DELIMITER &&
+ CREATE PROCEDURE Cleanup()
+ BEGIN
+ END &&
+ DELIMITER ;
+ 
+  DROP PROCEDURE IF EXISTS shut_down;
+					-- procedura di cleanup con annessa reset delle eventuali variabili e drop di eventi
+ DELIMITER &&
+ CREATE PROCEDURE SHUT_DOWN()
+ BEGIN 
+ 
+ END &
+ DELIMITER ;
+					-- procedura di load dei dati nelle varie tabelle
+DROP PROCEDURE IF EXISTS _Load;
+
+DELIMITER &&
+CREATE PROCEDURE _Load()
+BEGIN
+	insert into 
+END &
+DELIMITER ;
+ 
+			-- select con limit
+DROP PROCEDURE IF EXISTS _select;
+
+DELIMITER $$
+CREATE PROCEDURE _Select()
+BEGIN
+  IF (SELECT COUNT(*) FROM esercizi) < 1000
+     AND (SELECT COUNT(*) FROM professori) < 1000
+     AND (SELECT COUNT(*) FROM assegnazioni) < 1000
+     AND (SELECT COUNT(*) FROM studenti) < 1000
+     THEN
+	select * from esercizi;
+	select * from professori;
+	select * from assegnazioni;
+	select * from studenti;
+    else
+    select * from esercizi limit 1000;
+	select * from professori limit 1000;
+	select * from assegnazioni limit 1000;
+	select * from studenti limit 1000; 
+  END IF;
+END $$
+DELIMITER ;
+ 
+ CALL _SELECT()
+ 
+ 
+ 
+ 
